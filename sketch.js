86c2c1fe45c9c47a07b9fd877889b1b3c0bcf11f
rony ginosar <rony.ginosar@mail.huji.ac.il>
2020-11-09 15:15:44 +0200

    cleanup3

--- sketch.js
+++ sketch.js
@@ -1,15 +1,13 @@
-let w = [];
+var w = [];
 
-// TODO make input
-
-// let displayText = "מערכות משטחים"
-let displayText = "abcief ghdjkl";
+let displayText = "מערכות משטחים"
+// let displayText = "abcief ghdjkl";
 // let displayText = "abcief ghdjkl".toUpperCase();
 
 let bg;
 // let font;
-let logowidth;
-let logoheight;
+var logowidth;
+var logoheight;
 // let logowidth = 400;
 // let logoheight = 350;
 let margins = 10;
@@ -18,12 +16,13 @@ let fontMinWidth = 100;
 let fontMaxWidth = 500;
 let fontMinHeight = 100;
 let fontMaxHeight = 500;
-let widest = 500; // TODO test
-let narrowest = 100; // TODO test
+// Font changing global vars
+var widest;
+var narrowest;
+var widthList = [];
 var acc_width = 0;
-let minwidth = 50;
-// let widest;
-// let narrowest;
+// let minwidth = 50;
+
 
 let testcolors = ['rgb(0,0,255)', 'rgb(0,200,255)', 'rgb(100,200,100)',
 	'rgb(255,200,100)', 'rgb(100,0,100)', 'rgb(255,200,0)', 'rgb(255,0,100)',
@@ -43,17 +42,22 @@ function setup() {
 	bg = color('rgb(0,0,0)');
 	// bg = color('rgb(255,255,255)');
 	frameRate(1);
-}
 
-function draw() {
-	background(bg);
 	// 1. input text to array, word per row via space char
 	w = displayText.split(' ').slice();//.reverse() // for latin - remove ".slice().reverse()"
 	wordLenght = w[0].length;
+	// TODO make input
+}
+
+function draw() {
+	// inits for every loop
+	background(bg);
+	widthList = [];
 
 	// 2. turn into array of arrays, create divs for each letter
 	// for latin - remove .reverse()*2
 	// TODO make an array of divz?
+	// TODO - should the div creation happen in setup and just update sizes?
 	w.reverse().forEach((word, w_index) => {
 		w[w_index] = word.split('');
 		w[w_index].reverse().forEach((letter, l_index) => {
@@ -61,7 +65,7 @@ function draw() {
 			divName = div_index.toString();
 			var divName = createDiv(letter).id(divName);
 			// 3. create Letter object for each letter
-			
+
 			// lh = random(logoheight * 0.1, logoheight * 0.4);
 			lh = 50;
 			y = margins; // 1st row height
@@ -72,27 +76,25 @@ function draw() {
 					// acc_width update according to prev letter
 					acc_width += select('#' + (l_index-1).toString()).size().width;
 				}
-
-				// TODO: for HEBREW ??
-				// remove curr letter width
-				// lw -= divName.size().width;
+				// TODO is there a letter that is too narow to stretch?
+				// exclude it
 
 				// update lw regarding other letters before
 				// only relevant in first row hence here
 
-				// TODO : sort this logic!
 				// TODO: should be relyant on fontsize, not logowidth
 				minw = logowidth * 0.05 ;
 				maxw = max((logowidth - acc_width) * 0.4, logowidth * 0.1);
 				// maxmax makes sure no one is too narrow, we dont want logowidth - acc_width to be smaller than min
 
 				lw = int(random(minw, maxw));
-				// console.log(int(minw) , int(maxw), lw);
 
 				// last letter width fills to the end of logosize:
 				if(l_index == wordLenght-1){
 					lw = (logowidth - acc_width);
 				}
+
+				widthList.push(lw);
 			}
 
 			if (w_index >= 1){ // ammend params for later rows
@@ -102,6 +104,7 @@ function draw() {
 				// console.log("UP", upperSize.width , upperSize.height); // TEST PRINTS
 				y += upperSize.height;
 				lw = upperSize.width;
+
 				// update acc_width or init it for new row
 				if (l_index == 0){ acc_width = 0; }
 				else {
@@ -109,7 +112,6 @@ function draw() {
 				}
 			}
 			x = margins + acc_width;
-			// TODO: was x = width - margins - acc_width; is this needed for heb?
 
 			let l = new Letter(int(x), int(y), int(lw), int(lh), color(testcolors[div_index]), divName);
 			l.display();
@@ -120,6 +122,10 @@ function draw() {
 		});
 	});
 
+	// widest = max(widthList);
+	// narrowest = min(widthList);
+	// console.log(widest , narrowest);
+
 	// NOTES:
 	// keeping here for maybe later:
 	// bounds = font.textBounds(select('#'+(i.toString())).html(), 0, 0, fontsize);
@@ -129,17 +135,15 @@ function draw() {
 	// Dail back on the random.
 	// randomize select one letter to grow (not 2,6)
 	// all the rest - stay in place.
-	// widest = int(max(l1w, l2w, l3w, l4w));
-	// narrowest = int(min(l1w, l2w, l3w, l4w));
-	//
-	// TODO: here do animate
-	// noLoop();
+
+
+	noLoop();
 
 	// ----
 	// Export to SVG
 	// save("mySVG.svg"); // give file name
 	// print("saved svg");
-	noLoop(); // we just want to export once
+	// noLoop(); // we just want to export once
 
 }
 
@@ -155,35 +159,31 @@ class Letter {
 
 	animate(){
 		// TODO:
+
 	}
 
-	display(){
-		// test:
-		// noStroke();
-		// fill(this.c);
-		// rect(this.x, this.y, this.w, this.h);
-
-
-		// control DOM letters
-		// TODO fit text perfect!
-		// this.div.style('height', this.h +'px');
-		// this.div.style('line-height', this.h*0.37 +'px');
-		this.div.style('line-height', this.h +'px');
+	display(){ // control DOM letters
+		// div size and location
 		this.div.style('width', this.w +'px');
-		this.div.style('font-size', this.h +'px'); //this.h * 0.75;
+		this.div.style('height', this.h +'px');
+		this.div.position(this.x, this.y);
 		// this.div.style('font-size', this.h*1.35 +'px'); //this.h * 0.75;
+		// this.div.style('line-height', this.h*0.37 +'px');
+		// this.div.style('vertical-align','top');
 
+		// div font settings
+		this.div.style('line-height', this.h +'px');
+		this.div.style('font-size', this.h +'px'); //this.h * 0.75;
+		this.div.style('color', this.c);
+		// this.div.style('color', 'white'); // test
+		// this.div.style('background', this.c); // test
 
+		// map w to variable width
+		widest = max(widthList);
+		narrowest = min(widthList);
 		let wmap = map(this.w,narrowest,widest,fontMinWidth,fontMaxWidth,true);
 		wmap = "'wdth'"+' '+ int(wmap).toString();
-		// console.log(wmap);
 		this.div.style('font-variation-settings', wmap);
-		// this.div.style('vertical-align','top');
-		this.div.style('color', 'white');
-		this.div.style('background', this.c);
-		// this.div.style('color', "white");
-		// this.div.style('background', 'black');
-		this.div.position(this.x, this.y);
 	}
 // end of Letter class
 }
